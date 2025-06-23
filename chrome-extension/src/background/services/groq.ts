13078dfe6fec25e76bef4f7753c5a409fb029f65
Bojana Andonova <bojana.andonova03@gmail.com>
2025-06-23 18:30:49 +0200

    hiding api key as env variable

--- chrome-extension/src/background/services/groq.ts
+++ chrome-extension/src/background/services/groq.ts
@@ -2,7 +2,7 @@ import { ChatGroq } from '@langchain/groq';
 
 export function getGroqLLM() {
   return new ChatGroq({
-    apiKey: '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',
+    apiKey: import.meta.env.VITE_GROQ_API_KEY,
     model: 'llama3-70b-8192',
     temperature: 0.1,
     topP: 0.9,
